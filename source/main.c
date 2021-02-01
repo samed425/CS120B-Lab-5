8a6d520cc557a3e9b582437f622f43e9c04dcdf5
Bahman Baidar <bbaid001@wch136-22.cs.ucr.edu>
2021-01-31 19:17:44 -0800

    Part 2 next try

--- source/main.c
+++ source/main.c
@@ -12,112 +12,70 @@
 #include "simAVRHeader.h"
 #endif
 
-enum States{INIT, s0, s1, s2, s3, s4}state;
+enum States{INIT, wait, inc, dec, reset} state;
 unsigned char temp;
 unsigned char val;
 
 void Tick()
 {
-    switch(state)
-    {
-       case INIT:
-        state = s0;
-        break;
+    switch (state) {
+    case (INIT) :
+	val = 7;
+	PORTC = val;
+	break;
 
-        case s0:
-            if(temp == 0x01)
-            {
-              state = s1;
+    case (wait) :
+	PORTC = val;
+	break;
+    
+    case (inc) :
+	val = val + 1;
+	PORTC = val;
+	break;
 
-            }
-            else if(temp == 0x02)
-            {
-              state = s2;
-            }
-            else if(temp == 0x03)
-            {
-              state = s3;
-            }
-            else
-            {
-              state = s0;
-            }
-            break;
+    case (dec) :
+	val = val - 1;
+	break;
 
-        case s4:
-            if(temp == 0x00)
-            {
-              state = s0;
-            }
-            else if(temp == 0x03)
-            {
-              state = s3;
-            }
-            break;
+    case (reset) :
+	val = 0;
+	PORTC = val;
+	break;
 
-        case s1:
-            state = s4;
-            break;
-
-        case s2:
-            state = s4;
-            break;
-
-        case s3:
-            state = s4;
-            break;
-
-        default:
-            break;
-    }
-
-    switch(state) {
-
-        case INIT:
-            break;
-
-        case s0:
-            PORTC = val;
-            break;
-
-        case s4:
-            PORTC = val;
-            break;
-
-        case s1:
-            if(PORTC == 0x09)
-            {
-                PORTC = val;
-            }
-            else
-            {
-                PORTC = val + 0x01;
-                val = val + 0x01;
-            }
-            break;
-
-        case s2:
-            if(PORTC == 0x00)
-            {
-                PORTC = val;
-            }
-            else
-            {
-                PORTC = val - 0x01;
-                val = val - 0x01;
-            }
-            break;
-
-        case s3:
-            PORTC = 0x00;
-            val = 0x00;
-            break;
-
-        default:
-            break;
-    }
+    default:
+	break;    
 }
 
+    switch(state) {
+    case (INIT) : 
+	state = wait;
+	break;
+  
+    case (wait) :
+	if (PA0 && !PA1) {state = inc;}
+	else if (!PA0 && PA1) {state = dec;}
+	else if (PA0 && PA1) {state = reset;}
+	else {state = wait;}
+	break;
+
+    case (inc) :
+	state = wait;
+	break;
+
+    case (dec) :
+	state = wait;
+	break;
+
+    case (reset) :
+	val = 0;
+	PORTC = val;
+	break;
+
+    default:
+	state = INIT;
+	break;
+}
+}
 int main(void) {
     DDRA = 0x00; PORTA = 0xFF;
     DDRC = 0xFF; PORTC = 0x00;
@@ -127,7 +85,6 @@ int main(void) {
     
     while (1)
     {
-        temp = (~PINA & 0x03);
         Tick();
     }
 }
