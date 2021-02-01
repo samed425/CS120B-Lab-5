74cc033902f09fd787cd2223a3175212a80644f5
Bahman Baidar <bbaid001@wch136-22.cs.ucr.edu>
2021-01-31 21:40:25 -0800

    whatever part 3

--- source/main.c
+++ source/main.c
@@ -13,8 +13,6 @@
 #endif
 
 enum States{INIT, s0, s1, s2, s3} state;
-unsigned char temp;
-unsigned char val;
 
 void Tick()
 {
@@ -25,22 +23,22 @@ void Tick()
 	   break;
 
 	case (s0) :
-	   if (PORTA == 0x01) { state = s1; }
+	   if (PORTA) { state = s1; }
 	   else { state = s0; }
 	   break;
 
 	case (s1) :
-	   if (PORTA == 0x00) {state = s2; }
+	   if (!PORTA) {state = s2; }
 	   else { state = s1; }
 	   break;
 	
 	case (s2) :
-	   if (PORTA == 0x01) { state = s3; }
+	   if (!PORTA) { state = s3; }
 	   else { state = s2; }
 	   break;
 
 	case (s3) :
-	   if (PORTA == 0x00) { state = s0; }
+	   if (!PORTA) { state = s0; }
 	   else { state = s3; }
 	   break;
 
@@ -74,8 +72,6 @@ void Tick()
 int main(void) {
     DDRA = 0x00; PORTA = 0xFF;
     DDRB = 0xFF; PORTB = 0x00;
-
-    val = 0x00;
     state = INIT;
     
     while (1)
